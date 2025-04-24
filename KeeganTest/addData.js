95d0a1455d9d6318a34b8a42fe1e04bb95d31c45
Amras <64250162+Amras2322@users.noreply.github.com>
2025-04-24 11:51:23 -0500

    Working Examples

---
+++ KeeganTest/addData.js
@@ -0,0 +1,26 @@
+const fetch = require('isomorphic-fetch');
+
+const params = {
+  apiKey: 'JAt2QvcpexsoAC3aLsxoPHeyBKvCHiiG212XnbZTmFT25uZw7-Dn95grYIY',
+  spreadsheetId: '1R8FT2Ars9ZdW2aXULX-ppLMgU2tYmGcQxAPCyNPHwrw',
+};
+
+const url = new URL('https://api.sheetson.com/v2/sheets/Sheet1');
+Object.keys(params).forEach((key) =>
+  url.searchParams.append(key, encodeURIComponent(params[key])),
+);
+
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    Name: 'Bob',
+    Age: '34',
+    HairColor: 'Yellow',
+  }),
+})
+  .then((r) => r.json())
+  .then((result) => console.log(result))
+  .catch((err) => console.error('Error:', err));
